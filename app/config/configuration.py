1ddb41c3f521e647da5590987b71903d46860553
vkyl <viktor.kyliukh@gmail.com>
2025-03-21 01:00:18 +0200

    config reformated

--- app/config/configuration.py
+++ app/config/configuration.py
@@ -1,11 +1,10 @@
 from dotenv import load_dotenv
 from os import environ
 
-ROOT_PATH = './../.env'
-load_dotenv(ROOT_PATH)
+load_dotenv()
 
 
 class Config:
-    TEST_SECRET = environ.get('TEST_SECRET')
     WEATHER_API_KEY = environ.get('WEATHER_API_KEY')
+    WEATHER_API_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
 
--- app/core/consts.py
+++ app/core/consts.py
@@ -1,4 +1,4 @@
 from app.config import Config
 
 WEATHER_API_KEY = Config.WEATHER_API_KEY
-WEATHER_API_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
+WEATHER_API_URL = Config.WEATHER_API_URL
