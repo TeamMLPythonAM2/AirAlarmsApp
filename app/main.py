1fb01cab7c02a97f44ee72c82d0126d949be9d21
~ Yurii <locomoc.yt@gmail.com>
2025-04-27 19:28:31 +0300

    fix 7

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # from logs.logger import loggable, logger
 from app.routers.current_alarms_websocket import router_ws
 from app.routers.current_predict_endpoint import router_get
@@ -6,6 +7,13 @@ from app.routers.predict_for_all_hours_endpoint import router_task_7
 from app.routers.update_predictions import router_predict
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router_ws)
 app.include_router(router_get)
