3c6a946f487a173a6a1a4f542e0c6c68d06e245b
~ Yurii <locomoc.yt@gmail.com>
2025-04-27 20:08:09 +0300

    fix 9

--- app/routers/current_predict_endpoint.py
+++ app/routers/current_predict_endpoint.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Header
+from fastapi import APIRouter, Header, HTTPException
 from datetime import datetime, timedelta
 import pandas as pd
 from app.config.configuration import Config
@@ -9,17 +9,20 @@ router_get = APIRouter()
 
 
 def get_prediction(city, hour) -> bool:
-    datetime_now = datetime.now(tz=Config.KYIV_TZ).replace(minute=0, second=0, microsecond=0)
-    path_time = datetime_now.strftime("%Y-%m-%d_%H")
-    file_path = os.path.join(Config.HOURLY_PREDICTIONS_PATH, f"{path_time}.parquet")
-    prediction_now = pd.read_parquet(file_path)
-    required_time = datetime_now + timedelta(hours=hour)
-    required_time = required_time.replace(tzinfo=None)
-    prediction_now['datetime'] = prediction_now['datetime'].dt.tz_localize(None)
-    data_to_return = ((prediction_now[(prediction_now['city_address'] == city) &
-                                      (prediction_now['datetime'] == required_time)]
-                      .get("predict")))
-    return bool(data_to_return.values[0])
+    try:
+        datetime_now = datetime.now(tz=Config.KYIV_TZ).replace(minute=0, second=0, microsecond=0)
+        path_time = datetime_now.strftime("%Y-%m-%d_%H")
+        file_path = os.path.join(Config.HOURLY_PREDICTIONS_PATH, f"{path_time}.parquet")
+        prediction_now = pd.read_parquet(file_path)
+        required_time = datetime_now + timedelta(hours=hour)
+        required_time = required_time.replace(tzinfo=None)
+        prediction_now['datetime'] = prediction_now['datetime'].dt.tz_localize(None)
+        data_to_return = ((prediction_now[(prediction_now['city_address'] == city) &
+                                          (prediction_now['datetime'] == required_time)]
+                          .get("predict")))
+        return bool(data_to_return.values[0])
+    except Exception as e:
+        raise HTTPException(status_code=403, detail="Access forbidden")
 
 
 @router_get.get('/prediction')
